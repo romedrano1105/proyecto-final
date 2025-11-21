caa95e4503be5b8251afd1cc54577fa0086b4711
aabigailfloresc-create <aabigail.floresc@gmail.com>
2025-11-20 18:43:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import datos_del_inventario
 from operaciones import agregar_prenda, vender_prenda, consultar_existencias, mostrar_inventario
 
+# Interfaz del usuario
 def menu():
     while True:
         print("\n==============================")
         print("      SISTEMA DE INVENTARIO")
         print("==============================")
-        print("1. Agregar prenda")
-        print("2. Registrar venta")
+        print("1. Agregar prenda") # Se muestra cada una de las opciones
+        print("2. Registrar venta") # para facilitar la comprensión 
         print("3. Consultar existencias")
         print("4. Mostrar inventario")
         print("5. Ingresos del día")
         print("6. Salir")
 
         opcion = input("Seleccione una opción: ")
-
+# Funciones importadas
         if opcion == "1":
-            agregar_prenda()
+            agregar_prenda() #
         elif opcion == "2":
             vender_prenda()
         elif opcion == "3":
@@ -24,11 +25,11 @@ def menu():
         elif opcion == "4":
             mostrar_inventario()
         elif opcion == "5":
-            print(f"\nIngresos del día: ${datos_del_inventario.ingresos_del_dia:.2f}")
+            print(f"\nIngresos del día: ${datos_del_inventario.ingresos_del_dia:.2f}") # 
         elif opcion == "6":
-            break
+            break # La opcion 6 saca al usuario del menu
         else:
-            print("Opción inválida.")
+            print("Opción inválida.") # Se evitan posibles errores 
 
 if __name__ == "__main__":
     menu()
